d934862e0f74411e0646578f392cbbf27916495f
Michael Cirkl <MickenCZ@seznam.cz>
2025-05-08 16:58:21 +0200

    enhanced responsiveness

--- js/eventHandling/farmplotinteractions.js
+++ js/eventHandling/farmplotinteractions.js
@@ -17,6 +17,18 @@ document.querySelectorAll(".cell").forEach(cell => {
         const seedPrice = config.seeds.find(seed => seed.name == seedType).price;
         if (cell.dataset.occupied == "false" && new State().money >= seedPrice) {
             new Plant(cell, seedType) // creates it on the field
+        } else if (cell.dataset.occupied == "true") {
+            Toastify({
+                text: "The field cell is already occupied",
+                duration: 1500,
+                close: true
+            }).showToast()
+        } else {
+            Toastify({
+                text: "You don't have enough funds to plant this seed",
+                duration: 2000,
+                close: true
+            }).showToast()
         }
     });
 })
